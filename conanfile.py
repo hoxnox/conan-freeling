857c8216b7c95b6bac1197c2d3b62745a859d1fa
hoxnox <hoxnox@gmail.com>
2019-08-22 16:28:26 +0300

    WIP: freeling compiles

---
+++ conanfile.py
@@ -0,0 +1,53 @@
+import os
+from conans import CMake, tools
+from nxtools import NxConanFile
+from glob import glob
+
+class FreelingConan(NxConanFile):
+    name = "FreeLing"
+    version = "4.1"
+    fullname = "%s-%s" % (name, version)
+    license = "http://www.gnu.org/licenses/agpl-3.0.html"
+    author = "Lluis Padro"
+    url = "https://github.com/hoxnox/conan-freeling"
+    description = "FreeLing is a C++ library providing language analysis functionalities (morphological analysis, named entity detection, PoS-tagging, parsing, Word Sense Disambiguation, Semantic Role Labelling, etc.) for a variety of languages (English, Spanish, Portuguese, Italian, French, German, Russian, Catalan, Galician, Croatian, Slovene, among others)."
+    topics = ("nlp", "language", "text processing")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    build_policy = "missing"
+    requires = ("boost/1.66.0@hoxnox/stable",
+                "icu/64.2@hoxnox/stable",
+                "zlib/1.2.11@hoxnox/stable")
+
+    def config(self):
+        self.options["boost"].icu = True
+
+    def do_source(self):
+        tarball_name = "%s.tar.gz" % self.fullname
+        self.retrieve("ccb3322db6851075c9419bb5e472aa6b2e32cc7e9fa01981cff49ea3b212247e",
+                [
+                    "vendor://TALP-UPC/FreeLing/%s.tar.gz" % self.fullname,
+                    "https://github.com/TALP-UPC/FreeLing/releases/download/%s/%s"
+                        % (self.version, tarball_name)
+                ], tarball_name)
+        tools.untargz(tarball_name)
+        os.unlink(tarball_name)
+
+    def do_build(self):
+        cmake = CMake(self)
+        for file in sorted(glob("patch/[0-9]*.patch")):
+            self.output.info("Applying patch '{file}'".format(file=file))
+            tools.patch(base_path=self.fullname, patch_file=file, strip=0)
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.staging_dir
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.definitions["BOOST_ROOT"] = self.deps_cpp_info["boost"].rootpath
+        cmake.definitions["ZLIB_ROOT"] = self.deps_cpp_info["zlib"].rootpath
+        cmake.definitions["ICU_ROOT"] = self.deps_cpp_info["icu"].rootpath
+        cmake.verbose = True
+        self.output.info("cmake  cmd: %s" % cmake.command_line)
+        cmake.configure(source_folder=self.fullname)
+        cmake.build(target="install")
+
+    def do_package_info(self):
+        self.cpp_info.libs = ["freeling"]
---
+++ test_package/example.cpp
@@ -0,0 +1,71 @@
+#include <freeling.h>
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+freeling::maco_options
+my_maco_options(const wstring& lang, const wstring& path)
+{
+	freeling::maco_options opt(lang);
+	opt.UserMapFile     = L"";
+	opt.LocutionsFile   = L"";
+	opt.AffixFile       = path + L"locucions.dat";
+	opt.ProbabilityFile = path + L"probabilitats.dat";
+	opt.DictionaryFile  = path + L"dicc.src";
+	opt.NPdataFile      = path + L"np.dat";
+	opt.PunctuationFile = path + L"../common/punct.dat";
+	return opt;
+}
+
+int
+main(int argc, char* argv[])
+{
+	freeling::util::init_locale(L"default");
+	freeling::tokenizer tk(L"/tmp/freeling/share/freeling/en/tokenizer.dat");
+	freeling::splitter  sp(L"/tmp/freeling/share/freeling/en/splitter.dat");
+	freeling::maco      morfo(my_maco_options(L"en", L"/tmp/freeling/share/freeling/en/"));
+	morfo.set_active_options( //
+		false,                // UserMap
+		true,                 // NumberDetection
+		true,                 // PunctuationDetection
+		true,                 // DatesDetection
+		true,                 // DictionarySearch
+		true,                 // AffixAnalisys
+		false,                // CompoundAnalisys
+		true,                 // RtokContractions
+		false,                // MultiwordsDetection
+		true,                 // NERecognition
+		false,                // QuantitiesDetection
+		true);                // ProbabilityAssignment
+	freeling::hmm_tagger tagger(L"/tmp/freeling/share/freeling/en/tagger.dat", true, FORCE_TAGGER);
+
+	wstring              text = L"Dynamic authorization failed for device.";
+	list<freeling::word> lw   = tk.tokenize(text);
+	if (lw.size() == 0)
+		wcout << L"error" << endl;
+	list<freeling::sentence> ls = sp.split(lw);
+	morfo.analyze(ls);
+	tagger.analyze(ls);
+
+	for (list<freeling::sentence>::const_iterator is = ls.begin(); is != ls.end(); ++is)
+	{
+		for (freeling::sentence::const_iterator w = is->begin(); w != is->end(); ++w)
+		{
+			wcout << L"word '" << w->get_form() << L"'" << endl;
+			wcout << L" Possible analisys: {";
+			for (freeling::word::const_iterator a = w->analysis_begin(); a != w->analysis_end();
+				 ++a)
+			{
+
+				wcout << L" (" << a->get_lemma() << L"," << a->get_tag() << L")";
+			}
+			wcout << L" }" << endl;
+			wcout << L"  Selected analisys: (" << w->get_lemma() << L", " << w->get_tag() << L")"
+				  << endl;
+		}
+		wcout << endl;
+	}
+	return 0;
+}
